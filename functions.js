36b8f401fd541fb95899d3d07e745cc7d025b8f3
Lucas <laguero90@gmail.com>
2020-11-10 21:48:26 -0300

    funcion archivarcontacto

--- functions.js
+++ functions.js
@@ -1,11 +1,6 @@
-// Declaracion de variables GENERALES
 
-// contacto
 
-let nombre = "";
-let email = '' ;
-let asunto = '';
-let mensaje = '';
+
 
 
 // funciones constructoras
@@ -29,23 +24,57 @@ var popotas = new NuevaZapatilla ("zoo", "ojotas", '500', ['amarillo', 'verde', 
 
 // se puede hacer una pagina HTML especifica solo para cargar las zapatillas como variables y actualizar stocks y precios??
 
+//let variable = document.getElementById ('nombre dato a traer')
+
+// funcion para los contactos (aunque ya lo hace el HTML)
+
+// function contactopormail (nombre, email, asunto, mensaje){
+//     if (nombre !== "" || email !== "" || asunto !== "" || mensaje !== ""){
+//         alert ("Introduzca todos los campos: Nombre, Email, Asunto y su mensaje")
+//     }
+//     else {
+//         //enviar el mensaje
+//     }
+// }
 
 
-// funcion para los contactos
+// objeto para carrito que se mueve
 
-function contactopormail (nombre, email, asunto, mensaje){
-    if (nombre !== "" || email !== "" || asunto !== "" || mensaje !== ""){
-        alert ("Introduzca todos los campos: Nombre, Email, Asunto y su mensaje")
-    }
-    else {
-        //enviar el mensaje
-    }
+//var carrito = {
+//    carrito : carritoestatico,
+ //   carritoeliminado : carritoeliminado,
+//    carritoingreso : carritoingreso,
+//}
+
+// funcion para grabar los datos del email
+
+// falta lo del boton "Enviar" no se como hacerlo aun
+//linea 156 del HTML  <form action="/action_page.php" target="_blank">
+
+
+
+
+
+function archivarcontactopormail (){
+debugger
+
+var nombre = document.getElementById ("nombre").value
+var email = document.getElementById ("email").value
+var asunto = document.getElementById ("asunto").value
+var mensaje = document.getElementById ("mensaje").value
+    localStorage.setItem ('nombre1',nombre);
+    localStorage.setItem ('email1',email);
+    localStorage.setItem ('asunto1',asunto);
+    localStorage.setItem ('mensaje1',mensaje);
 }
 
-
-
-
-
+function enviaralservidorelcontacto (){
+    localStorage.getItem ('nombre1');
+    localStorage.getItem ('email1');
+    localStorage.getItem ('asunto1');
+    localStorage.getItem ('mensaje1');
+    //poner aca enviar esto al servidor (ver luego como)
+}
 
 
 
