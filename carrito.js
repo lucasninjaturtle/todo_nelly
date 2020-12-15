b98595c863d3c57821e46871b3303b9b229a6ebe
Lucas <laguero90@gmail.com>
2020-12-15 15:32:01 -0300

    manejo del ajax

--- carrito.js
+++ carrito.js
@@ -5,7 +5,7 @@ const agregarcarrito = function (){
     return console.log("hola")
 //hacer logica de que no puede haber mas del mismo pedido con un ID
 //acumular en el localstorage la compra para lanzarla a otra pagina tipo legacy (./carrito)
-
+// Ver de usar alguna API de wsp para abrirlo con los datos de un objeto de la compra e importalo en el chat.
 }
 
 
